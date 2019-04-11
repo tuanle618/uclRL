6f108916a912147b1c2e85426369cffb7765e6f0
ptl93 <tuan.le@campus.lmu.de>
2019-04-11 14:59:05 +0200

    add every-visit MC policy evaluation

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -11,7 +11,7 @@ import numpy as np
 class Agent:    
     def __init__(self, env, discount_factor=1.0, epsilon=1e-6):
         """
-        Initialized an Agent to solve a MDP via model free Monte-Carlo method.
+        Initialized an Agent to solve a MDP via model free Monte-Carlo Policy Evaluation method.
         Params:
             env [OpenAI env.]
                 env.P represents the transition probabilities of the environment:
@@ -56,9 +56,7 @@ class Agent:
             state = next_state
             
             
-            
-        
-    def first_visit_MC(self, num_iter=10000, discount_factor=None):
+    def monte_carlo(self, num_iter=10000, discount_factor=None, first_visit=True):
         """
         
         """
@@ -79,21 +77,25 @@ class Agent:
             reversed_episode = episode[::-1]
             for t, step in enumerate(reversed_episode):
                 G = discount_factor*G + step["reward"]
-                # Make sure its first-visit:
-                if step["state"] not in [x["state"] for x in episode[:t]]:
+                if first_visit:
+                    # Make sure its first-visit:
+                    if step["state"] not in [x["state"] for x in episode[:t]]:                   
+                        returns[step["state"]].append(G)
+                        new_value = np.average(returns[step["state"]])
+                        v_fnc[step["state"]] = new_value
+                else: #every-visit
                     returns[step["state"]].append(G)
                     new_value = np.average(returns[step["state"]])
                     v_fnc[step["state"]] = new_value
-        
+                                
         return v_fnc
-    
-    def every_visit_MC(self, num_iter=10000, discount_factor=None):
-        pass
+
         
 
     
 
 def main():
+    np.random.seed(26)
     env = GridworldEnv(shape=[4,4])
     agent = Agent(env)
     ## Sample one episode
@@ -101,11 +103,20 @@ def main():
     print("Example: Sample episode for Monte Carlo:")
     print(episode)
     ## Do First-Visit-Monte-Carlo
-    first_visit_MC_value_fnc = agent.first_visit_MC()
+    first_visit_MC_value_fnc = agent.monte_carlo(first_visit=True,
+                                                 discount_factor=1.0,
+                                                 num_iter=10000)
     first_visit_MC_value_fnc = np.round(first_visit_MC_value_fnc, 2)
     print("Result first-visit Monte Carlo:")
     print(first_visit_MC_value_fnc.reshape((env.shape)))
-    
+
+    ## Do Every-Visit Monte-Carlo
+    every_visit_MC_value_fnc = agent.monte_carlo(first_visit=False,
+                                                 discount_factor=1.0,
+                                                 num_iter=10000)
+    every_visit_MC_value_fnc = np.round(every_visit_MC_value_fnc, 2)
+    print("Result every-visit Monte Carlo:")
+    print(every_visit_MC_value_fnc.reshape((env.shape)))
     
 if __name__ == "__main__":
     main()
